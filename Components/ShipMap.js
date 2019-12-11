7c52127d8bae4da99f114a206a86865a11951ff3
helloImHai <1503.hai.nguyen@gmail.com>
2019-12-11 11:53:16 +0800

    =Add map

--- Components/ShipMap.js
+++ Components/ShipMap.js
@@ -1,8 +1,13 @@
-    /**
-     * Initialise BMap
-     */
-    var map = new BMap.Map("container");
-    // create a map instance
-    var point = new BMap.Point(116.404, 39.915);
-    // Create point coordinates   
-    map.centerAndZoom(point, 11);
+/**
+ * Initialise BMap
+ */
+var map = new BMap.Map("ship-map");
+alert("running");
+// create a map instance
+var point = new BMap.Point(116.404, 39.915);
+// Create point coordinates   
+map.centerAndZoom(point, 11);
+map.addControl(new BMap.NavigationControl());
+map.addControl(new BMap.ScaleControl());
+map.addControl(new BMap.OverviewMapControl());
+map.addControl(new BMap.MapTypeControl());  
