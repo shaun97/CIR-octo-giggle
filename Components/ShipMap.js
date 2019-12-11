35e848cb2ef904870b86fc0a042ba447878ec975
helloImHai <1503.hai.nguyen@gmail.com>
2019-12-11 23:36:59 +0800

    Empty box added, need to populate

--- Components/ShipMap.js
+++ Components/ShipMap.js
@@ -1,7 +1,9 @@
 /**
  * Initialise BMap
  */
-var map = new BMap.Map("ship-map");
+let mapDiv = document.getElementById("ship-map");
+
+var map = new BMap.Map(mapDiv);
 
 // create a map instance
 // var point = new BMap.Point(116.404, 39.915);
@@ -9,16 +11,33 @@ var point = new BMap.Point(121.4737, 31.2304);
 var convertor = new BMap.Convertor();
 convertor.translate([point], 1, 5, (data) => {
   console.log(data.status);
-  map.centerAndZoom(point, 11);
+  map.centerAndZoom(point, 10);
   map.addControl(new BMap.NavigationControl());
   map.addControl(new BMap.ScaleControl());
   map.addControl(new BMap.OverviewMapControl());
-  map.addControl(new BMap.MapTypeControl());  
+  map.addControl(new BMap.MapTypeControl());
 })
 
-// Create point coordinates   
-// map.centerAndZoom(point, 11);
-// map.addControl(new BMap.NavigationControl());
-// map.addControl(new BMap.ScaleControl());
-// map.addControl(new BMap.OverviewMapControl());
-// map.addControl(new BMap.MapTypeControl());  
+/**
+ * Adding a point
+ */
+var shipPt = new BMap.Point(122.020, 31.650);
+convertor.translate([shipPt], 1, 5, (data) => {
+  let shipMarker = new BMap.Marker(shipPt);
+
+  map.addOverlay(shipMarker);
+
+  shipMarker.addEventListener("click", function(){    
+    // alert("Clicked on the ship");
+    map.centerAndZoom(shipPt, 10);
+    if (document.getElementById("pop-up-ship-info").hidden) {
+      document.getElementById("pop-up-ship-info").hidden = false;
+    } else {
+      document.getElementById("pop-up-ship-info").hidden = true;
+    }
+    
+});  
+})
+
+
+
