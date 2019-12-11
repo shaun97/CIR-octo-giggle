3360fa9e0ae309e4a37c65ee1c6b1dc6b9857dc5
helloImHai <1503.hai.nguyen@gmail.com>
2019-12-11 16:06:15 +0800

    =Bootstrap v3

--- Components/ShipMap.js
+++ Components/ShipMap.js
@@ -2,12 +2,23 @@
  * Initialise BMap
  */
 var map = new BMap.Map("ship-map");
-alert("running");
+
 // create a map instance
-var point = new BMap.Point(116.404, 39.915);
+// var point = new BMap.Point(116.404, 39.915);
+var point = new BMap.Point(121.4737, 31.2304);
+var convertor = new BMap.Convertor();
+convertor.translate([point], 1, 5, (data) => {
+  console.log(data.status);
+  map.centerAndZoom(point, 11);
+  map.addControl(new BMap.NavigationControl());
+  map.addControl(new BMap.ScaleControl());
+  map.addControl(new BMap.OverviewMapControl());
+  map.addControl(new BMap.MapTypeControl());  
+})
+
 // Create point coordinates   
-map.centerAndZoom(point, 11);
-map.addControl(new BMap.NavigationControl());
-map.addControl(new BMap.ScaleControl());
-map.addControl(new BMap.OverviewMapControl());
-map.addControl(new BMap.MapTypeControl());  
+// map.centerAndZoom(point, 11);
+// map.addControl(new BMap.NavigationControl());
+// map.addControl(new BMap.ScaleControl());
+// map.addControl(new BMap.OverviewMapControl());
+// map.addControl(new BMap.MapTypeControl());  
