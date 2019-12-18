24ff6448cf396971fd1d1181ac5da1ef6de58e81
Shaun Teo <xshaun97@gmail.com>
2019-12-18 15:20:24 +0800

    buttons for fitrate

--- Components/FiltrateBox.js
+++ Components/FiltrateBox.js
@@ -1,5 +1,93 @@
 $(document).ready(function () {
+  //Close Button
   $("#filtrate-close-button").click(function () {
     $("#ship-filtrate-box").hide();
   });
+
+  //Filtrate selector
+  $("#selector-all-button").click(function () {
+    $(this).toggleClass("grey-button");
+    if (!$(this).hasClass("grey-button")) {
+      $("#selector-clear-button").addClass("grey-button");
+      $(".filtrate-type-button").removeClass("grey-button");
+    }
+  });
+
+  //
+  $("#selector-clear-button").click(function () {
+    $(this).toggleClass("grey-button");
+    if (!$(this).hasClass("grey-button")) {
+      $("#selector-all-button").addClass("grey-button");
+      $(".filtrate-type-button").addClass("grey-button");
+    }
+  });
+
+  //Type buttons
+  $("#option-type-freighter-btn").click(function () {
+    $(this).toggleClass("grey-button");
+    if ($(this).hasClass("grey-button")) {
+      $("#selector-all-button").addClass("grey-button");
+    } else {
+      $("#selector-clear-button").addClass("grey-button");
+    };
+  })
+
+
+  $("#option-type-container-btn").click(function () {
+    $(this).toggleClass("grey-button");
+    if ($(this).hasClass("grey-button")) {
+      $("#selector-all-button").addClass("grey-button");
+    } else {
+      $("#selector-clear-button").addClass("grey-button");
+    };
+  })
+
+
+  $("#option-type-oil-btn").click(function () {
+    $(this).toggleClass("grey-button");
+    if ($(this).hasClass("grey-button")) {
+      $("#selector-all-button").addClass("grey-button");
+    } else {
+      $("#selector-clear-button").addClass("grey-button");
+    };
+  })
+
+  $("#option-type-tugboat-btn").click(function () {
+    $(this).toggleClass("grey-button");
+    if ($(this).hasClass("grey-button")) {
+      $("#selector-all-button").addClass("grey-button");
+    } else {
+      $("#selector-clear-button").addClass("grey-button");
+    };
+  })
+
+  $("#option-type-fish-btn").click(function () {
+    $(this).toggleClass("grey-button");
+    if ($(this).hasClass("grey-button")) {
+      $("#selector-all-button").addClass("grey-button");
+    } else {
+      $("#selector-clear-button").addClass("grey-button");
+    };
+  })
+
+  $("#option-type-passenger-btn").click(function () {
+    $(this).toggleClass("grey-button");
+    if ($(this).hasClass("grey-button")) {
+      $("#selector-all-button").addClass("grey-button");
+    } else {
+      $("#selector-clear-button").addClass("grey-button");
+    };
+  })
+
+  $("#option-type-others-btn").click(function () {
+    $(this).toggleClass("grey-button");
+    if ($(this).hasClass("grey-button")) {
+      $("#selector-all-button").addClass("grey-button");
+    } else {
+      $("#selector-clear-button").addClass("grey-button");
+    };
+  })
+
+
 });
+
