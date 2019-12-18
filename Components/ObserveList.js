6f32c22a978ed31df84244450076990079b62361
helloImHai <1503.hai.nguyen@gmail.com>
2019-12-18 15:20:32 +0800

    Change tree list in observe bar

--- Components/ObserveList.js
+++ Components/ObserveList.js
@@ -1,3 +1,8 @@
+layui.use(['element', 'layer'], function () {
+  var element = layui.element;
+  var layer = layui.layer;
+});
+
 $(document).ready(function () {
   $("#observe-close-button").click(function () {
     $("#observe-list-box").hide();
