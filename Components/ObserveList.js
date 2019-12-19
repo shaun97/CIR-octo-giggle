98418c56f0d0ad242e333b1b404ae5232bc663cf
helloImHai <1503.hai.nguyen@gmail.com>
2019-12-20 00:46:38 +0800

    More functionalities for fleet list

--- Components/ObserveList.js
+++ Components/ObserveList.js
@@ -1,5 +1,3 @@
-
-
 $(document).ready(function () {
   $("#observe-close-button").click(function () {
     $("#observe-list-box").hide();
@@ -8,35 +6,11 @@ $(document).ready(function () {
     $("#observe-write").show();
     $("#observe-read").hide();
   });
-  $(".tree-button").click(function (e) {
-    e.stopPropagation();
-  });
-  const fleetNames = [];
-  let len = 0;
-  $("#add-fleet-btn").click(function (e) {
+  $("#add-fleet-btn").click(function () {
     let newFleetName = $("#add-ship-fleet-name").val();
     if (newFleetName == "") { return; }
-    $("#add-ship-fleet-name").val("");
-    let i = 0;
-    for (i = 0; i < len; i++) {
-      if (newFleetName == fleetNames[i]) { break; }
-    }
-    let eye = $('<button/>').addClass("tree-button").html('<img src="./Images/icon_hide.png" class="tree-button-icon">');
-    let track = $('<button/>').addClass("tree-button").html('<img src="./Images/icon_track_myship_track.png" class="tree-button-icon">');
-    let edit = $('<button/>').addClass("tree-button").html('<img src="./Images/icon_edit_myship_track.png" class="tree-button-icon">');
-    let close = $('<button/>').addClass("tree-button").html('<img src="./Images/icon_delt_myship_track.png" class="tree-button-icon">');
-    let treeButtons = $('<div/>').addClass("tree-buttons").append(eye, track, edit, close);
-    let ship = $('<div/>').addClass("ship-in-list").html($("#ship-info-name").text()).append(treeButtons);
 
-    //-------------- FUNCTIONALITY FOR SHIP TREE-BUTTONS --------------//
-
-    close.click(function () {
-      ship.remove();
-    });
-
-    //-------------- FUNCTIONALITY FOR SHIP TREE-BUTTONS --------------//
-    if (i == len) {
-      let showing = false;
+    if (!$(`#content-list-${newFleetName.replace(" ", "")}`).length) {
       let newFleet = $('<h2/>').addClass("layui-colla-title").text(newFleetName);
       let icon = $('<i/>').addClass("layui-icon").addClass("layui-colla-icon").html("");
       let eyeF = $('<button/>').addClass("tree-button").html('<img src="./Images/icon_hide.png" class="tree-button-icon">');
@@ -44,25 +18,23 @@ $(document).ready(function () {
       let editF = $('<button/>').addClass("tree-button").html('<img src="./Images/icon_edit_myship_track.png" class="tree-button-icon">');
       let closeF = $('<button/>').addClass("tree-button").html('<img src="./Images/icon_delt_myship_track.png" class="tree-button-icon">');
       let treeButtonsF = $('<div/>').addClass("tree-buttons").append(eyeF, tableF, editF, closeF);
-      let content = $('<div/>').addClass("layui-colla-content").attr("id", `content-list-${i}`).append(ship);
+      let content = $('<div/>').addClass("layui-colla-content").attr("id", `content-list-${newFleetName.replace(" ", "")}`);
       let newItem = $('<div/>').addClass("layui-colla-item")
         .append(newFleet.append(treeButtonsF).append(icon))
         .append(content)
       $('.my-ship-list').append(newItem);
-      len = fleetNames.push(newFleetName);
 
       //-------------- FUNCTIONALITY FOR FLEET TREE-BUTTONS --------------//
 
       newFleet.click(function () {
-        if (!showing) {
-          content.addClass("layui-show");
-          icon.html("");
-        } else {
-          content.attr("class", "layui-colla-content");
-          icon.html("")
-        }
-        showing = !showing;
+        content.toggleClass("layui-show");
       })
+
+      eyeF.click(function () {
+        eyeF.html() == '<img src="./Images/icon_open.png" class="tree-button-icon">'
+          ? eyeF.html('<img src="./Images/icon_hide.png" class="tree-button-icon">')
+          : eyeF.html('<img src="./Images/icon_open.png" class="tree-button-icon">');
+      });
 
       closeF.click(function () {
         newItem.empty();
@@ -75,12 +47,45 @@ $(document).ready(function () {
 
       //-------------- END FUNCTIONALITY FOR FLEET TREE-BUTTONS --------------//
 
-    } else {
-      $(`#content-list-${i}`).append(ship);
-    }
+    } 
+
+    $(".tree-button").click(function (e) {
+      e.stopPropagation();
+    });
   });
+  $("#add-ship-to-fleet-btn").click(function() {
+    const fleetName = $("#add-ship-fleet-name").val();
+    if (!fleetName) {
+      alert("Choose or add a fleet name");
+      return;
+    }
+    $("#add-fleet-btn").click();
+    let nickname = $("#add-ship-to-fleet-inputbar").val();
+    let eye = $('<button/>').addClass("tree-button").html('<img src="./Images/icon_hide.png" class="tree-button-icon">');
+    let track = $('<button/>').addClass("tree-button").html('<img src="./Images/icon_track_myship_track.png" class="tree-button-icon">');
+    let edit = $('<button/>').addClass("tree-button").html('<img src="./Images/icon_edit_myship_track.png" class="tree-button-icon">');
+    let close = $('<button/>').addClass("tree-button").html('<img src="./Images/icon_delt_myship_track.png" class="tree-button-icon">');
+    let treeButtons = $('<div/>').addClass("tree-buttons").append(eye, track, edit, close);
+    let ship = $('<div/>').addClass("ship-in-list").html(nickname ? nickname : $("#ship-info-name").text()).append(treeButtons);
+    $(`#content-list-${fleetName.replace(" ", "")}`).append(ship);
+    //-------------- FUNCTIONALITY FOR SHIP TREE-BUTTONS --------------//
+
+    eye.click(function () {
+      eye.html() == '<img src="./Images/icon_open.png" class="tree-button-icon">'
+        ? eye.html('<img src="./Images/icon_hide.png" class="tree-button-icon">')
+        : eye.html('<img src="./Images/icon_open.png" class="tree-button-icon">');
+    });
+
+    close.click(function () {
+      ship.remove();
+    });
+
+    //-------------- END FUNCTIONALITY FOR SHIP TREE-BUTTONS --------------//
+
+  })
 });
 
+// Needed for checkbox 
 layui.use(['element', 'layer', 'form'], function () {
   var element = layui.element;
   var layer = layui.layer;
