3e4eed72f51d693a8e3f61d9f807d1a65c1ac7b8
helloImHai <1503.hai.nguyen@gmail.com>
2019-12-20 11:04:10 +0800

    Before deconflict

--- Components/ShipMap.js
+++ Components/ShipMap.js
@@ -10,42 +10,45 @@ var map = new BMap.Map(mapDiv);
 var point = new BMap.Point(121.4737, 31.2304);
 var convertor = new BMap.Convertor();
 convertor.translate([point], 1, 5, (data) => {
-  map.centerAndZoom(point, 9);
+  map.centerAndZoom(point, 1);
   map.addControl(new BMap.ScaleControl());
   map.addControl(new BMap.OverviewMapControl());
 })
 
+// 600000
+json = getAllShips();
+arr = json.data;
 /**
  * Adding a point
  */
-for (let i = 0; i < json.length; i++) {
-  let pt = new BMap.Point(json[i].lng, json[i].lat);
-  convertor.translate([pt], 1, 5, (data) => {
-    let ship = new BMap.Marker(pt);
-    map.addOverlay(ship);
-    ship.addEventListener("click", function () {
-      map.centerAndZoom(pt, 9);
-      const { name, nickname, idM, len, country, idL, idS, mass, lng, lat, img } = json[i];
-      console.log(img);
-      $(document).ready(function () {
-        $("#ship-info-box").show();
-        $("#ship-info-nknm").text(nickname);
-        $("#ship-info-name").text(name);
-        $("#ship-info-id-m").text(idM);
-        $("#ship-info-length").text(len);
-        $("#ship-info-country1").text(country);
-        $("#ship-info-country2").text(country);
-        $("#ship-info-id-l").text(idL);
-        $("#ship-info-id-s").text(idS);
-        $("#ship-info-mass").text(mass);
-        $("#ship-info-lng").text(lng);
-        $("#ship-info-lat").text(lat);
-        $("#ship-info-img").attr("src", img);
-      });
+// for (let i = 0; i < arr.length; i += 5) {
+//   // const { name, nickname, idM, len, country, idL, idS, mass, lng, lat, img } = json[i];
+//   const {LONGITUDE, LATITUDE} = arr[i];
+//   let pt = new BMap.Point(LONGITUDE, LATITUDE);
+//   convertor.translate([pt], 1, 5, (data) => {
+//     let ship = new BMap.Marker(pt);
+//     map.addOverlay(ship);
+//     ship.addEventListener("click", function () {
+//       map.centerAndZoom(pt, 5);
+//       $(document).ready(function () {
+//         // $("#ship-info-box").show();
+//         // $("#ship-info-nknm").text(nickname);
+//         // $("#ship-info-name").text(name);
+//         // $("#ship-info-id-m").text(idM);
+//         // $("#ship-info-length").text(len);
+//         // $("#ship-info-country1").text(country);
+//         // $("#ship-info-country2").text(country);
+//         // $("#ship-info-id-l").text(idL);
+//         // $("#ship-info-id-s").text(idS);
+//         // $("#ship-info-mass").text(mass);
+//         $("#ship-info-lng").text(LONGITUDE);
+//         $("#ship-info-lat").text(LATITUDE);
+//         // $("#ship-info-img").attr("src", img);
+//       });
 
-    });
-  })
-}
+//     });
+//   })
+// }
 
 
 
