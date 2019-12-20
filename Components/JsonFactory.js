4f0a7b1f1b8d48370bfe5239e703bd1e3cc44087
Shaun Teo <xshaun97@gmail.com>
2019-12-20 11:05:13 +0800

    search for ships json factory

--- Components/JsonFactory.js
+++ Components/JsonFactory.js
@@ -12,9 +12,21 @@ function getAllShips() {
     return json;
 }
 
-var test = getAllShips();
-console.log(test);
-
 function searchForShip(MmsiIorName) {
+    var query = "http://192.168.0.113:8761/TestController/getMMSI?MmsiIorName=" + MmsiIorName;
 
+    var json = null;
+    $.ajax({
+        'async': false,
+        'global': false,
+        'url': query,
+        'dataType': "json",
+        'success': function (data) {
+            json = data;
+        }
+    });
+    return json;
 }
+
+var test = searchForShip(232003233); 
+console.log(test);
