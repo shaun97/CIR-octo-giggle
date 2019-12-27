67d1d64a01224d9888c88ee8dc663ceef506093c
helloImHai <1503.hai.nguyen@gmail.com>
2019-12-27 11:32:43 +0800

    add functionalities for other filters

--- Components/FiltrateBox.js
+++ Components/FiltrateBox.js
@@ -1,16 +1,8 @@
 let TYPE_ARR = Array(9).fill(true);
-
-let SIZE_40 = true;
-let SIZE_80 = true;
-let SIZE_120 = true;
-let SIZE_160 = true;
-let SIZE_240 = true;
-let SIZE_360 = true;
-let SIZE_360_ABV = true;
-let SIZE_CUSTOM = false;
-
-let MOVING = true;
-let STOPPED = true;
+let TYPE_FLTR = true;
+let SIZE_ARR = Array(8).fill(true);
+let SIZE_FLTR = true;
+let MOVE_ARR = Array(2).fill(true);
 
 let PRED = null;
 
@@ -193,9 +185,24 @@ $(document).ready(function () {
     TYPE_ARR[6] = $("#option-type-military-btn").hasClass("grey-button") ? false : true;
     TYPE_ARR[7] = $("#option-type-others-btn").hasClass("grey-button") ? false : true;
     TYPE_ARR[8] = $("#option-type-unknown-btn").hasClass("grey-button") ? false : true;
-    let boo = TYPE_ARR.reduce((x, y) => x && y, true);
-    // console.log(boo, TYPE_ARR);
-    filterShips(boo);
+
+    SIZE_ARR[0] = $('#option-size-40-btn').hasClass("grey-button") ? false : true;
+    SIZE_ARR[1] = $('#option-size-80-btn').hasClass("grey-button") ? false : true;
+    SIZE_ARR[2] = $('#option-size-120-btn').hasClass("grey-button") ? false : true;
+    SIZE_ARR[3] = $('#option-size-160-btn').hasClass("grey-button") ? false : true;
+    SIZE_ARR[4] = $('#option-size-240-btn').hasClass("grey-button") ? false : true;
+    SIZE_ARR[5] = $('#option-size-320-btn').hasClass("grey-button") ? false : true;
+    SIZE_ARR[6] = $('#option-size-320abv-btn').hasClass("grey-button") ? false : true;
+    SIZE_ARR[7] = $('#option-size-custom').hasClass("grey-button") ? false : true;
+
+    MOVE_ARR[0] = $('#option-status-stop-btn').hasClass("grey-button") ? false : true;
+    MOVE_ARR[1] = $('#option-status-moving-btn').hasClass("grey-button") ? false : true;
+
+    TYPE_FLTR = TYPE_ARR.reduce((x, y) => x && y, true);
+    // console.log(TYPE_FLTR, TYPE_ARR);
+    SIZE_FLTR = SIZE_ARR.reduce((x, y) => x && y, true);
+    // console.log(SIZE_FLTR, SIZE_ARR);
+    filterShips();
   })
 });
 
@@ -227,49 +234,53 @@ function getShipIcon(ship) {
     return './img/othertype.png';
   } else {
     ship.TYPE_IDX = 8;
-    
+
     return './img/unknown.png';
   }
 }
 
-function filterShips(boo) {
+function setFilterProperties(ship) {
+  var size = ship.A;
+  if (!size || size < 40) {
+    ship.SIZE_IDX = 0;
+  } else if (size < 80) {
+    ship.SIZE_IDX = 1;
+  } else if (size < 120) {
+    ship.SIZE_IDX = 2;
+  } else if (size < 160) {
+    ship.SIZE_IDX = 3;
+  } else if (size < 240) {
+    ship.SIZE_IDX = 4;
+  } else if (size < 320) {
+    ship.SIZE_IDX = 5;
+  } else {
+    ship.SIZE_IDX = 6;
+  }
+}
+
+function filterShips() {
   var data = [];
   if (!ALL_SHIPS) {
     alert("Please refresh page");
     return;
   }
-
-  if (boo) { // No filter
-    for (var i = 0; i < ALL_SHIPS.length; i++) {
+  let thisShip = null;
+  for (var i = 0; i < ALL_SHIPS.length; i++) {
+    thisShip = ALL_SHIPS[i];
+    if (customPred(thisShip)) {
       var img = new Image(0.1, 0.05);
-      img.src = getShipIcon(ALL_SHIPS[i]);
+      img.src = getShipIcon(thisShip);
+      setFilterProperties(thisShip);
       data.push({
         geometry: {
           type: 'Point',
-          coordinates: [ALL_SHIPS[i]['LONGITUDE1'], ALL_SHIPS[i]['LATITUDE1']],
+          coordinates: [thisShip['LONGITUDE1'], thisShip['LATITUDE1']],
           id: i,
         },
         icon: img,
-        deg: ALL_SHIPS[i].HEADING - 90,
-        data: ALL_SHIPS[i],
+        deg: thisShip.HEADING - 90,
+        data: thisShip,
       });
-    }
-  } else {
-    for (var i = 0; i < ALL_SHIPS.length; i++) {
-      if (customPred(ALL_SHIPS[i])) {
-        var img = new Image(0.1, 0.05);
-        img.src = getShipIcon(ALL_SHIPS[i]);
-        data.push({
-          geometry: {
-            type: 'Point',
-            coordinates: [ALL_SHIPS[i]['LONGITUDE1'], ALL_SHIPS[i]['LATITUDE1']],
-            id: i,
-          },
-          icon: img,
-          deg: ALL_SHIPS[i].HEADING - 90,
-          data: ALL_SHIPS[i],
-        });
-      }
     }
   }
   // 船只
@@ -278,14 +289,16 @@ function filterShips(boo) {
   var dataSet = new mapv.DataSet(data);
   // 添加百度地图可视化叠加图层 The OVERLAY OPTIONS!!!
   var options = {
-    // zIndex: 0, // 层级
-    // unit:'px',
-    // fillStyle: '#df4c06',
-    // shadowColor: 'rgba(255, 50, 50, 1)',
-    // shadowBlur: 80,
-    // styleType: 'stroke',
-    // globalCompositeOperation: 'darker',
-    // lineWidth: 0,
+    /*
+    zIndex: 0, // 层级
+    unit:'px',
+    fillStyle: '#df4c06',
+    shadowColor: 'rgba(255, 50, 50, 1)',
+    shadowBlur: 80,
+    styleType: 'stroke',
+    globalCompositeOperation: 'darker',
+    lineWidth: 0,
+    // */
     methods: { // 一些事件回调函数
       click: function (item) { // 点击事件，返回对应点击元素的对象值
         if (item == null) return;
@@ -311,25 +324,36 @@ function filterShips(boo) {
 
 function customPred(ship) {
   const typeIdx = ship.TYPE_IDX;
-  if (!TYPE_ARR[0] && typeIdx == 0) {
-    return false;
-  } else if (!TYPE_ARR[1] && typeIdx == 1) {
-    return false;
-  } else if (!TYPE_ARR[2] && typeIdx == 2) {
-    return false;
-  } else if (!TYPE_ARR[3] && typeIdx == 3) {
-    return false;
-  } else if (!TYPE_ARR[4] && typeIdx == 4) {
-    return false;
-  } else if (!TYPE_ARR[5] && typeIdx == 5) {
-    return false;
-  } else if (!TYPE_ARR[6] && typeIdx == 6) {
-    return false;
-  } else if (!TYPE_ARR[7] && typeIdx == 7) {
-    return false;
-  } else if (!TYPE_ARR[8] && typeIdx == 8) {
-    return false;
-  } else {
-    return true;
+  const sizeIdx = ship.SIZE_IDX;
+  const speed = ship.SOG;
+
+  if (!TYPE_FLTR) {
+    switch (typeIdx) {
+      case 0: if (!TYPE_ARR[0]) return false;
+      case 1: if (!TYPE_ARR[1]) return false;
+      case 2: if (!TYPE_ARR[2]) return false;
+      case 3: if (!TYPE_ARR[3]) return false;
+      case 4: if (!TYPE_ARR[4]) return false;
+      case 5: if (!TYPE_ARR[5]) return false;
+      case 6: if (!TYPE_ARR[6]) return false;
+      case 7: if (!TYPE_ARR[7]) return false;
+      case 8: if (!TYPE_ARR[8]) return false;
+    }
   }
+  if (!SIZE_FLTR) {
+    switch (sizeIdx) {
+      case 0: if (!SIZE_ARR[0]) return false;
+      case 1: if (!SIZE_ARR[1]) return false;
+      case 2: if (!SIZE_ARR[2]) return false;
+      case 3: if (!SIZE_ARR[3]) return false;
+      case 4: if (!SIZE_ARR[4]) return false;
+      case 5: if (!SIZE_ARR[5]) return false;
+      case 6: if (!SIZE_ARR[6]) return false;
+    }
+  }
+  
+  if (!MOVE_ARR[0] && speed < 0.5 || !speed) return false;
+  if (!MOVE_ARR[1] && speed >= 0.5) return false;
+  // switch 
+  return true;
 }
