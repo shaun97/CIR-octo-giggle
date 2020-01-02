ba9cc91d1d678babd416e7dad0ee85338b403a5e
helloImHai <1503.hai.nguyen@gmail.com>
2020-01-02 11:21:32 +0800

    pop-up on top

--- View/Draggable.js
+++ View/Draggable.js
@@ -5,4 +5,24 @@ $(document).ready(function () {
         stack: ".pop-up",
         cursor:"move",
     });
+    $("#ship-info-box").click(() => {
+        console.log('hello');
+        $(".pop-up").css('z-index', '1');
+        $("#ship-info-box").css('z-index', '2');
+    });
+    $("#ship-filtrate-box").click(() => {
+        console.log('hello');
+        $(".pop-up").css('z-index', '1');
+        $("#ship-filtrate-box").css('z-index', '2');
+    });
+    $("#observe-list-box").click(() => {
+        console.log('hello');
+        $(".pop-up").css('z-index', '1');
+        $("#observe-list-box").css('z-index', '2');
+    });
+    $("#change-name-box").click(() => {
+        console.log('hello');
+        $(".pop-up").css('z-index', '1');
+        $("#change-name-box").css('z-index', '2');
+    });
 });
