920854b96b09d8cbfacb5e6fe11603d1182935c5
helloImHai <1503.hai.nguyen@gmail.com>
2020-01-10 15:19:03 +0800

    Global comments

--- Modal/Globals.js
+++ Modal/Globals.js
@@ -1,28 +1,39 @@
 const IP_ADDRESS = '117.78.28.83:8761';
 // const IP_ADDRESS = '192.168.0.181:8761';
 
+let USER_KEY = '10';
+
+// ALL_SHIPS : Array of Geo items (查看vmap API). 在initMap处填写.
 var ALL_SHIPS = [];
+
+// CURRENT_SHIPS : Array of Geo items (查看vmap API).
 var CURRENT_SHIPS
+
+// B_Map Map
 var MAP = null;
 
-//States
-var MAPV_LAYER;
-var CLEAR_LAYER;
-var ZOOM_SHIP_OFFSET = 12;
-
-var BOAT_MARKERS = [];
+// 视图有两个主要状态：“MAP_VIEW”和“TRACK_VIEW”。这在“MAP_VIEW”上是“true”
 var MAP_VIEW = true;
+
+// vmap层
+var MAPV_LAYER;
+var CLEAR_LAYER; 
+
+
 var THIS_SHIP_ITEM = null;
 var THIS_SHIP_ITEM_HOVER = null;
 var THIS_SHIP_LABEL = null;
 var THIS_SHIP_LABEL_HOVER = null;
 
+// Array of data of a single ship. Used for track.
 var HISTORY_DATA = null;
 var TRACK_MARKERS = [];
 
 const FLEETS = {};
 let FLEET_NAME_LIST = [];
 
+// 筛选状态
+var ZOOM_SHIP_OFFSET = 12;
 let TYPE_ARR = Array(9).fill(true);
 let TYPE_FLTR = true;
 let SIZE_ARR = Array(8).fill(true);
@@ -31,7 +42,5 @@ let MOVE_ARR = Array(2).fill(true);
 
 let SPEED_CHART = null;
 
-let USER_KEY = '10';
-
-// Very stupid
+// 检查是否添加了复选框功能
 let CHECKBOX_FUNCTIONALITY = false;
